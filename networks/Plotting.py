2358679f119ca38c9f5abd266754df3bbaf59c6d
Graeme-George <16gfg2@queensu.ca>
2022-04-06 16:10:59 -0400

    Finished Plotting Program

--- networks/Plotting.py
+++ networks/Plotting.py
@@ -1,42 +1,77 @@
+
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import numpy as np
 
-def animate(i):
-    x.append(i)
-    y.append(R[i])
-
-    plt.clear()
-    plt.plot(x, y)
-    plt.axhline(1000,color='b')
-    plt.xlim([0,200])
-    plt.ylim([0,100000])
-    plt.legend('Current Balance','Starting Balance')
-    
-
-
-x=[]
-y=[]
+even = 6E9
+x_data=[]
+y_data=[]
 R=[]
+colors = ['g']
+bounds = [0]
 
-
-with open('20220329_RNNTraining.txt') as f:
+with open('networks/20220329_RNNTraining.txt') as f:
     lines = f.readlines()
+
 for i in range(len(lines)):
     R.append(float(lines[i]))
 
-#print(lines)
-# print(len(R))
-# fig = plt.figure(figsize=((10,8)))
-#ani = FuncAnimation(fig,animate,frames=200,interval=100)
-#ani.save(r'RewardPlot.gif')
 
-for i in range(200):
-    x.append(i)
-    y.append(R[i])
-    plt.clf()
-    plt.xlim(0,200)
-    plt.ylim(0,max(R))
-    plt.pause(0.001)
+for i in range(1,len(R)):
+    if R[i]<R[i-1]:
+        if colors[-1] != 'r':
+            colors.append('r')
+            bounds.append(i)
+    else:
+        if colors[-1] != 'g':
+            colors.append('g')
+            bounds.append(i)
+    
+if bounds[-1] != 199:
+    bounds.append(199)
+
+#print(bounds)
+#print(colors)
+cmap = ListedColormap(colors)
+
+fig, ax = plt.subplots()
+ax.clear()
+#ax.axhline(even,color='b')
+ax.set_xlim(0,200)
+ax.set_ylim(0,max(R))
+#ax.legend('Current Balance','Starting Balance')
+#line, = ax.plot(0,0)
+
+
+norm = BoundaryNorm(bounds, cmap.N)
+line = LineCollection([], cmap=cmap,norm=norm)
+line.set_array(np.linspace(0,199,200))
+ax.add_collection(line)
+
+
+
+def animate(i):
+    x_data.append(i)
+    y_data.append(R[i])
+
+    points = np.array([x_data, y_data]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    line.set_segments(segments)
+
+    # line.set_xdata(x_data)
+    # line.set_ydata(y_data)
+    # if R[i]<R[i-1]:
+    #     line.set_color('red')
+    # else:
+    #     line.set_color('green')
+    
+    return line,
+    
+
+ani=FuncAnimation(fig,func=animate,frames=np.arange(0,199,1),interval=100,repeat=False)
+ani.save(r'TestAnimation2.gif')
 
 plt.show()
 
