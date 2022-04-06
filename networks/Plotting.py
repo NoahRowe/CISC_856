982344f88cb04372953d4a66ef98b85a012f2930
Graeme-George <16gfg2@queensu.ca>
2022-04-06 13:50:00 -0400

    plotting updates

--- networks/Plotting.py
+++ networks/Plotting.py
@@ -5,25 +5,38 @@ def animate(i):
     x.append(i)
     y.append(R[i])
 
-    ax.clear()
-    ax.plot(x, y)
-    #ax.axhline(1000,color='b')
-    ax.set_xlim([0,300])
-    ax.set_ylim([0,100000])
-    ax.legend('Current Balance','Starting Balance')
+    plt.clear()
+    plt.plot(x, y)
+    plt.axhline(1000,color='b')
+    plt.xlim([0,200])
+    plt.ylim([0,100000])
+    plt.legend('Current Balance','Starting Balance')
+    
+
 
 x=[]
 y=[]
 R=[]
+
+
 with open('20220329_RNNTraining.txt') as f:
     lines = f.readlines()
 for i in range(len(lines)):
     R.append(float(lines[i]))
 
 #print(lines)
-#print(R)
-fig, ax = plt.subplots()
+# print(len(R))
+# fig = plt.figure(figsize=((10,8)))
+#ani = FuncAnimation(fig,animate,frames=200,interval=100)
+#ani.save(r'RewardPlot.gif')
 
+for i in range(200):
+    x.append(i)
+    y.append(R[i])
+    plt.clf()
+    plt.xlim(0,200)
+    plt.ylim(0,max(R))
+    plt.pause(0.001)
 
-ani = FuncAnimation(fig,animate,frames=300,interval=500,repeat=False)
 plt.show()
+
